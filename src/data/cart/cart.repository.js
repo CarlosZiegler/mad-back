cc1a5a539e30689a317f7319e2f21b73d1d0676b
carlosziegler <carlos.ziegler@gmail.com>
2021-03-22 19:35:23 +0100

    add c

--- src/data/cart/cart.repository.js
+++ src/data/cart/cart.repository.js
@@ -1,30 +1,33 @@
 const Cart = require('./cart.model');
 const { findProductById } = require('../product/product.repository');
 
-const findCartById = async id => {
-  return await Cart.findOne({ _id: id });
+const findCartById = async (id) => {
+  return await Cart.findOne({ _id: id }).populate('productId');
 };
 const getAllCarts = async () => {
   return await Cart.find();
 };
 const getCart = async () => {
-  const result = await Cart.findOne();
+  const result = await Cart.findOne().populate({
+    path: 'products.productId',
+  });
+
   return await result;
 };
-const createCart = async cart => {
+const createCart = async (cart) => {
   return await Cart.create(cart);
 };
 const updateCart = async (id, data) => {
   return await Cart.updateOne({ _id: id }, data);
 };
-const deleteCart = async id => {
+const deleteCart = async (id) => {
   return await Cart.deleteOne({ _id: id });
 };
 
-const getPricesFromListOfProducts = async products => {
+const getPricesFromListOfProducts = async (products) => {
   try {
     return await Promise.all(
-      products.map(async product => {
+      products.map(async (product) => {
         const found = await findProductById(product.productId);
         return found.price * product.qtd;
       })
